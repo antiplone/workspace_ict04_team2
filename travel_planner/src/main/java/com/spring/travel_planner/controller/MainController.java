7da3a7159e5de795f1a27398a5e99b7ed6952153
dAZI <antiplone@gmail.com>
2025-02-03 10:56:34 +0900

    [env,feat] settings for sts console. add comment for dev-orientation.

--- travel_planner/src/main/java/com/spring/travel_planner/controller/MainController.java
+++ travel_planner/src/main/java/com/spring/travel_planner/controller/MainController.java
@@ -3,6 +3,13 @@ package com.spring.travel_planner.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * [ <a href="https://doc.clickup.com/9018815828/d/h/8ct0dam-2398/41d697b5b23533c">개발방향 정리 페이지</a> ] <br>
+ * 주소를 Ctrl + 왼쪽클릭하면, 정리 페이지로 이동.
+ * 
+ * @author YeongDae.Jeon
+ *
+ */
 @Controller
 public class MainController {
 
