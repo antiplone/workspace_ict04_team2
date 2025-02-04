95fa4d343471b1d5c8d109586f3bca57ed07bcd5
jiyeon <hjybd1004@naver.com>
2025-02-04 10:03:08 +0900

    first source update_5

---
+++ travel_planner/src/main/java/com/spring/travel_planner/dto/RecommendCourseDTO.java
@@ -0,0 +1,12 @@
+package com.spring.travel_planner.dto;
+
+public class RecommendCourseDTO {
+	
+	private String ti_language;		// 언어
+	private String ti_thema;		// 관광타입
+	private String ti_service; 		// 서비스타입
+	private String ti_area;			// 지역
+	private String ti_area_detail;  // 지역 대표정보
+	
+	
+}
