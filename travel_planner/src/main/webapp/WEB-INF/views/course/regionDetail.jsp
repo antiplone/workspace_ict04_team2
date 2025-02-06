a63a22a455034a9277a4ccc22cd3468b342eeaa3
jiyeon <hjybd1004@naver.com>
2025-02-06 15:55:13 +0900

    "recommendcourse update_1"

---
+++ travel_planner/src/main/webapp/WEB-INF/views/course/regionDetail.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>지역 소개</title>
+</head>
+<body>
+
+<!-- 모달팝업 -->
+<div class="modal">
+	<div class="modal_popup">
+		<h3>모달 팝업 타이틀 입니다.</h3>
+		<p></p>	
+	</div>	
+</div>
+
+</body>
+</html>
---
+++ travel_planner/src/main/webapp/resources/js/common/main.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+
+	const toggleBtn = document.querySelector(".navbar_toggleBtn");
+	const menu= document.querySelector(".navbar_menu");
+	const icons= document.querySelector(".navbar_icons");
+	const menulists= document.querySelectorAll(".navbar_menu li");
+	const menulista= document.querySelectorAll(".navbar_menu li.active");
+	
+	console.log(menulists);
+	 
+	/*hamburger bar toggle button*/
+	toggleBtn.addEventListener('click', ()=>{
+		menu.classList.toggle('active');
+		icons.classList.toggle('active');
+	})
+	
+	
+	
+	/*navbar menu active button*/
+	menulists.forEach((menulist, index) => {
+		menulist.addEventListener("click",function(){
+	      if (this.classList.contains("active")) {
+	      } 
+	      else {
+        	this.classList.add("active");
+	      }
+      });
+    });
+	    
+	    
+	// 추천 여행지
+	   	function recomment_tour_list(){  
+	   		// alert("recomment_tour_list()");
+	   		$.ajax({
+	   			url:'${path}/recomment_tour_list.do', // 컨트롤러로 이동(9)
+	   			type: 'POST',
+	   			data : 'board_num=${dto.b_num}',
+	   			success : function(result){ // 콜백함수(13) => result는 comment_list
+	   				
+					$('#recomment_tour_list').html(result);
+	   			},
+	   			error:function(j,t, errorThrown){
+	   				alert(errorThrown);
+	   			},
+	   		});
+	   	}
+	   	
+})
